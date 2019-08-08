423d45ebf7c461aa31c57a46cc5df96a37c8d889
hwsknudsen <hwsknudsen@users.noreply.github.com>
2019-08-08 10:43:15 -0300

    Add files via upload

---
+++ printer.ps1
@@ -0,0 +1,22 @@
+﻿$printername = "\\viking-print16\Front"
+
+$printers = get-printer;
+$index = 0;
+foreach ($printer in $printers){
+    if ($printer.Name -eq $printername)
+        {
+            #Write-Host $printer.Name + $index;
+            break;
+        }
+    $index = $index + 1;
+}
+
+
+
+$registryPath = "HKCU:\Software\ABC System\General"
+New-ItemProperty -Path $registryPath -Name "PrinterIndex" -Value $index -PropertyType DWORD -Force | Out-Null
+
+
+#also set login id
+#[HKEY_CURRENT_USER\Software\ABC System\Login Dialog]
+#"Last Logged User"="jackie"
